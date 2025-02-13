823d3cc8a436f53dec8942814b5f7d274932c07f
sami1031 <sami03063260160@gmail.com>
2025-02-13 13:19:05 +0500

    push

--- lib/view/auth/forgot_screen.dart
+++ lib/view/auth/forgot_screen.dart
@@ -1,21 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
 import 'package:to_do_app/constants/app_icons.dart';
 import 'package:to_do_app/constants/app_images.dart';
-import 'package:to_do_app/main.dart';
+
 import 'package:to_do_app/view/auth/login_screen.dart';
 import 'package:to_do_app/widgets/buttons/commonbutton.dart';
 import 'package:to_do_app/widgets/fields/common_textfield.dart';
 
-class ForgotScreen extends StatelessWidget {
+class ForgotScreen extends StatefulWidget {
   const ForgotScreen({super.key});
 
   @override
+  State<ForgotScreen> createState() => _ForgotScreenState();
+}
+
+class _ForgotScreenState extends State<ForgotScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController emailController = TextEditingController();
-    final TextEditingController forgotController = TextEditingController();
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -39,16 +47,47 @@ class ForgotScreen extends StatelessWidget {
             SizedBox(height: 40.h),
             Image.asset(AppImages.addScreen),
             SizedBox(height: 50.h),
-            CommonTextfield(
-                hintText: 'Forgot Password', controller: forgotController),
+            Form(
+              key: _formKey,
+              child: CommonTextfield(
+                  validator: (value) {
+                    if (value == '' || value == null) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  hintText: 'Forgot Password',
+                  controller: emailController),
+            ),
             SizedBox(height: 50.h),
             Commonbutton(
+                isloading: loading,
                 titel: 'Forgot',
                 onTap: () async {
-                  await FirebaseAuth.instance
-                      .sendPasswordResetEmail(email: emailController.text);
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                  if (_formKey.currentState!.validate()) {
+                    try {
+                      setState(() {
+                        loading = true;
+                      });
+                      await FirebaseAuth.instance
+                          .sendPasswordResetEmail(email: emailController.text);
+
+                      Get.to(LoginScreen());
+
+                      setState(() {
+                        loading = false;
+                      });
+                      Get.snackbar('succes', 'Link send',
+                          backgroundColor: Colors.green);
+                    } on FirebaseAuthException catch (e) {
+                      Get.snackbar('Error', 'an error ocured',
+                          backgroundColor: Colors.red);
+
+                      setState(() {
+                        loading = false;
+                      });
+                    }
+                  }
                 })
           ],
         ),
--- lib/widgets/buttons/commonbutton.dart
+++ lib/widgets/buttons/commonbutton.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/constants/app_colors.dart';
 
-class Commonbutton extends StatefulWidget {
+class Commonbutton extends StatelessWidget {
   final String titel;
   final VoidCallback onTap;
-  const Commonbutton({super.key, required this.titel, required this.onTap});
+  final bool? isloading;
+  const Commonbutton(
+      {super.key,
+      required this.titel,
+      required this.onTap,
+      this.isloading = false});
 
-  @override
-  State<Commonbutton> createState() => _CommonbuttonState();
-}
-
-class _CommonbuttonState extends State<Commonbutton> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: widget.onTap,
+      onTap: onTap,
       child: Container(
         height: 44,
         width: 220,
         child: Center(
-          child: Text(
-            widget.titel,
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white),
-          ),
+          child: isloading == true
+              ? CircularProgressIndicator(
+                  backgroundColor: AppColors.color1,
+                )
+              : Text(
+                  titel,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
         ),
         decoration: BoxDecoration(color: AppColors.color1),
       ),
